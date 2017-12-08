a9d896fa3d4f54d4eed08476d5d4c1a0a8876e2e
Samueljoli <samuel.joli.ftn@gmail.com>
2017-12-08 10:16:10 -0500

    Set base, complete implementation tests

---
+++ test/implementation.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const Lab = require('lab');
+const { script, assertions } = Lab;
+const lab = exports.lab = script();
+const { describe, it } = lab;
+assertions.should();
+
+const Lib = require('../lib');
+
+describe('Implementation', () => {
+
+    it('should be immutable', () => {
+
+        const { string } = Lib;
+
+        const firstSchema = string();
+        const secondSchema = firstSchema.min(4);
+
+        firstSchema._tests.length.should.equal(0);
+        secondSchema._tests.length.should.equal(1);
+    });
+
+    it('should be able to validate itself', () => {
+
+        const { string } = Lib;
+        const ast = { kind: 'StringValue', value: '123' };
+
+        string().min(2).validate(ast).should.equal('123');
+    });
+});
